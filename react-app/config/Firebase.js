3329b8caddecf1b43425c94332030dc3e7a0e5e2
Prin-P <prinpulkes@gmail.com>
2024-04-09 14:28:30 -0400

    Create Firebase.js

---
+++ react-app/config/Firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAhYlzawTuTZjU8LX5zWZFx2P8CFdrtJNs",
+  authDomain: "recipe-generator-cs178.firebaseapp.com",
+  projectId: "recipe-generator-cs178",
+  storageBucket: "recipe-generator-cs178.appspot.com",
+  messagingSenderId: "980313247503",
+  appId: "1:980313247503:web:00087e384543e26fdeb87a",
+  measurementId: "G-PYG94JNJ7V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
