9a0f0677768604e8c734ef457c3801cc9e389902
Jessica Li <jessicali1703@gmail.com>
2024-04-16 23:54:11 -0400

    dialog + list

--- react-app/src/components/foodList.js
+++ react-app/src/components/foodList.js
@@ -5,10 +5,10 @@ function FoodList() {
         <div className="food-list"> 
             <Accordion className="AccordionRoot" type="multiple">
                 <AccordionItem value="proteins">
-                        <div className="AccordionInfo sticky-line">
-                            Proteins
-                            <AccordionTrigger className="first-level" />
-                        </div>
+                    <div className="AccordionInfo sticky-line">
+                        Proteins
+                        <AccordionTrigger className="first-level" />
+                    </div>
                     <AccordionContent>
                         <div className="AccordionInfo2">
                             list of proteins
@@ -20,53 +20,65 @@ function FoodList() {
                         Vegetables
                         <AccordionTrigger className="first-level" />
                     </div>
-                <AccordionContent>
-                    list of vegetables
-                </AccordionContent>
+                    <AccordionContent>
+                        <div className="AccordionInfo2">
+                            list of vegetables
+                        </div>
+                    </AccordionContent>
                 </AccordionItem>
                 <AccordionItem value="fruits">
-                        <div className="AccordionInfo sticky-line">
-                            Fruits
-                            <AccordionTrigger className="first-level" />
-                        </div>
+                    <div className="AccordionInfo sticky-line">
+                        Fruits
+                        <AccordionTrigger className="first-level" />
+                    </div>
                     <AccordionContent>
-                        list of fruits
+                        <div className="AccordionInfo2">
+                            list of fruits
+                        </div>
                     </AccordionContent>
                 </AccordionItem>
                 <AccordionItem value="dairy">
-                        <div className="AccordionInfo sticky-line">
-                            Dairy
-                            <AccordionTrigger className="first-level" />
-                        </div>
+                    <div className="AccordionInfo sticky-line">
+                        Dairy
+                        <AccordionTrigger className="first-level" />
+                    </div>
                     <AccordionContent>
-                        list of dairy
+                        <div className="AccordionInfo2">
+                            list of dairy
+                        </div>
                     </AccordionContent>
                 </AccordionItem>
                 <AccordionItem value="grains">
-                        <div className="AccordionInfo sticky-line">
-                            Grains
-                            <AccordionTrigger className="first-level" />
-                        </div>
+                    <div className="AccordionInfo sticky-line">
+                        Grains
+                        <AccordionTrigger className="first-level" />
+                    </div>
                     <AccordionContent>
-                        list of grains
+                        <div className="AccordionInfo2">
+                            list of grains
+                        </div>
                     </AccordionContent>
                 </AccordionItem>
                 <AccordionItem value="basic">
-                        <div className="AccordionInfo sticky-line">
-                            Basic Ingredients
-                            <AccordionTrigger className="first-level" />
-                        </div>
+                    <div className="AccordionInfo sticky-line">
+                        Basic Ingredients
+                        <AccordionTrigger className="first-level" />
+                    </div>
                     <AccordionContent>
-                        list of basic
+                        <div className="AccordionInfo2">
+                            list of basic ingredients
+                        </div>
                     </AccordionContent>
                 </AccordionItem>
                 <AccordionItem value="other">
-                        <div className="AccordionInfo sticky-line">
-                            Other
-                            <AccordionTrigger className="first-level" />
-                        </div>
+                    <div className="AccordionInfo sticky-line">
+                        Other
+                        <AccordionTrigger className="first-level" />
+                    </div>
                     <AccordionContent>
-                        list of other
+                        <div className="AccordionInfo2">
+                            list of other
+                        </div>
                     </AccordionContent>
                 </AccordionItem>
             </Accordion>
--- react-app/src/pages/cabinet.js
+++ react-app/src/pages/cabinet.js
@@ -1,3 +1,4 @@
+import { Dialog, DialogTrigger, DialogPortal, DialogOverlay, DialogContent, DialogCloseButton } from "../components/dialog";
 import FoodList from "../components/foodList";
 
 function Cabinet() {
@@ -7,8 +8,19 @@ function Cabinet() {
       <div className="row">
         <div className="col-6 text-center">insert svg here</div>
         <div className="col-6 text-center">
-            <button type="button" class="btn btn-success">add grocery haul</button>
-            <FoodList />
+          <Dialog>
+            <DialogTrigger>
+                <button type="button" class="btn btn-success">add grocery haul</button>
+            </DialogTrigger>
+            <DialogPortal>
+                <DialogOverlay className="DialogOverlay"/>
+                <DialogContent className="DialogContent">
+                    hihihihi
+                    <DialogCloseButton className="IconButton"/>
+                </DialogContent>
+            </DialogPortal>
+          </Dialog>
+          <FoodList />
         </div>
       </div>
 
